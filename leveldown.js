c5c72e1de6329e84f180df664023718dcb6286ab
Lars-Magnus Skog <ralphtheninja@riseup.net>
2015-05-10 21:10:15 +0200

    LevelDOWN -> LevelDOWNHyper

--- leveldown.js
+++ leveldown.js
@@ -7,58 +7,58 @@ const util              = require('util')
     , Iterator          = require('./iterator')
 
 
-function LevelDOWN (location) {
-  if (!(this instanceof LevelDOWN))
-    return new LevelDOWN(location)
+function LevelDOWNHyper (location) {
+  if (!(this instanceof LevelDOWNHyper))
+    return new LevelDOWNHyper(location)
 
   AbstractLevelDOWN.call(this, location)
   this.binding = binding(location)
 }
 
-util.inherits(LevelDOWN, AbstractLevelDOWN)
+util.inherits(LevelDOWNHyper, AbstractLevelDOWN)
 
 
-LevelDOWN.prototype._open = function (options, callback) {
+LevelDOWNHyper.prototype._open = function (options, callback) {
   this.binding.open(options, callback)
 }
 
 
-LevelDOWN.prototype._close = function (callback) {
+LevelDOWNHyper.prototype._close = function (callback) {
   this.binding.close(callback)
 }
 
 
-LevelDOWN.prototype._put = function (key, value, options, callback) {
+LevelDOWNHyper.prototype._put = function (key, value, options, callback) {
   this.binding.put(key, value, options, callback)
 }
 
 
-LevelDOWN.prototype._get = function (key, options, callback) {
+LevelDOWNHyper.prototype._get = function (key, options, callback) {
   this.binding.get(key, options, callback)
 }
 
 
-LevelDOWN.prototype._del = function (key, options, callback) {
+LevelDOWNHyper.prototype._del = function (key, options, callback) {
   this.binding.del(key, options, callback)
 }
 
 
-LevelDOWN.prototype._chainedBatch = function () {
+LevelDOWNHyper.prototype._chainedBatch = function () {
   return new ChainedBatch(this)
 }
 
 
-LevelDOWN.prototype._batch = function (operations, options, callback) {
+LevelDOWNHyper.prototype._batch = function (operations, options, callback) {
   return this.binding.batch(operations, options, callback)
 }
 
 
-LevelDOWN.prototype._approximateSize = function (start, end, callback) {
+LevelDOWNHyper.prototype._approximateSize = function (start, end, callback) {
   this.binding.approximateSize(start, end, callback)
 }
 
 
-LevelDOWN.prototype.getProperty = function (property) {
+LevelDOWNHyper.prototype.getProperty = function (property) {
   if (typeof property != 'string')
     throw new Error('getProperty() requires a valid `property` argument')
 
@@ -66,12 +66,12 @@ LevelDOWN.prototype.getProperty = function (property) {
 }
 
 
-LevelDOWN.prototype._iterator = function (options) {
+LevelDOWNHyper.prototype._iterator = function (options) {
   return new Iterator(this, options)
 }
 
 
-LevelDOWN.destroy = function (location, callback) {
+LevelDOWNHyper.destroy = function (location, callback) {
   if (arguments.length < 2)
     throw new Error('destroy() requires `location` and `callback` arguments')
 
@@ -85,7 +85,7 @@ LevelDOWN.destroy = function (location, callback) {
 }
 
 
-LevelDOWN.repair = function (location, callback) {
+LevelDOWNHyper.repair = function (location, callback) {
   if (arguments.length < 2)
     throw new Error('repair() requires `location` and `callback` arguments')
 
@@ -99,4 +99,4 @@ LevelDOWN.repair = function (location, callback) {
 }
 
 
-module.exports = LevelDOWN
+module.exports = LevelDOWNHyper
