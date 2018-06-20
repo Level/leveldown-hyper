8b37ed033066a018b908d30e0f1c4b2053809074
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-06-20 16:43:22 +0200

    Fix indentation

--- test/port-libuv-fix-test.js
+++ test/port-libuv-fix-test.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 test('test port-libuv is being used', function (t) {
   var version = fs.readFileSync(path.join(__dirname, '../deps/leveldb/leveldb.gyp'), 'utf8')
-        .match(/'ldbversion': '([^']+)'/)[1]
+    .match(/'ldbversion': '([^']+)'/)[1]
   t.equal(version, 'hyper', 'matched current leveldb version')
 
   // TODO 'deps/leveldb/leveldb-hyper/port/port.h does not include 'port_uv.h'
